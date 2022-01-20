97c17d1dea29264ecb71282632865fbfe621f1cf
Safronov Evgeniy <safronov.evgenijon@mail.ru>
2022-01-20 19:04:09 +0300

    Complete TextEditor

---
+++ src/RBPO_avtomatom_5_na_exam/MyHighlightPainter.java
@@ -0,0 +1,11 @@
+package RBPO_avtomatom_5_na_exam;
+
+import javax.swing.text.DefaultHighlighter;
+import java.awt.*;
+
+class MyHighlightPainter extends DefaultHighlighter.DefaultHighlightPainter {
+
+    public MyHighlightPainter(Color color) {
+        super(color);
+    }
+}
